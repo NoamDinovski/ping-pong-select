64d1b75de92601b5a0b367eb38065d13f35b41e8
NoamDinovski <noam.dinovski@gmail.com>
2021-01-31 09:22:12 +0200

    Support selection of many sockets by selecting in chunks

--- ping_pong_server.py
+++ ping_pong_server.py
@@ -11,6 +11,7 @@ Created: 28/01/2020
 
 import select
 import socket
+import sys
 
 ANY_INTERFACE = ""
 PORT = 1337
@@ -24,6 +25,8 @@ MAX_CONNECTIONS = 100
 POLL_TIMEOUT = 0.0
 
 BUFFER_SIZE = 1024
+
+MAX_FDS = 512
 
 
 def log_connection(connection_socket, message):
@@ -47,6 +50,32 @@ def close_connection(connections, connection_socket):
     connection_socket.close()
     connections.remove(connection_socket)
 
+def is_poll_object_supported():
+    return getattr(select, "poll", None) is not None
+
+def select_for_read(sockets, timeout):
+    length = len(sockets)
+    reads = []
+    while length > 0:
+        reads_temp, _, _ = select.select(sockets[max(0, length - MAX_FDS):length], [], [], timeout)
+        reads += reads_temp
+        length -= MAX_FDS
+    return reads
+
+
+def poll_for_read(poll_object, timeout):
+    poll_object.poll()
+
+
+def select_for_write(sockets, timeout):
+    length = len(sockets)
+    writes = []
+    while length > 0:
+        _, writes_temp, _ = select.select([], sockets[max(0, length - MAX_FDS):length], [], timeout)
+        writes += writes_temp
+        length -= MAX_FDS
+    return writes
+
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -57,8 +86,13 @@ def main():
 
         clients = []
 
+        use_poll = is_poll_object_supported()
+
+        if use_poll:
+            poll = select.poll()
+
         while True:
-            readable, _, _ = select.select([sock], [], [], POLL_TIMEOUT)
+            readable = select_for_read([sock], POLL_TIMEOUT)
             if readable:
                 client_socket = sock.accept()[0]
                 print(f"Accepted connection from {client_socket.getpeername()} "
@@ -69,7 +103,7 @@ def main():
             if not clients:
                 continue
 
-            readable_connections, _, _ = select.select(clients, [], [], POLL_TIMEOUT)
+            readable_connections = select_for_read(clients, POLL_TIMEOUT)
             connection_data = []
             for client_socket in clients:
                 if client_socket in readable_connections:
@@ -81,23 +115,28 @@ def main():
                         close_connection(clients, client_socket)
                         readable_connections.remove(client_socket)
 
+            connections_to_pop = []
+
             for index, data in enumerate(connection_data):
                 log_connection(readable_connections[index], f"Received message: {data}")
                 if data != CLIENT_MESSAGE:
                     log_connection(readable_connections[index], f"Invalid message. Closing connection.")
                     close_connection(clients, readable_connections[index])
-                    readable_connections.pop(index)
+                    connections_to_pop.append(readable_connections[index])
+
+            readable_connections = [conn for conn in readable_connections if conn not in connections_to_pop]
 
             if not readable_connections:
                 continue
 
-            _, writable_connections, _ = select.select([], readable_connections, [], POLL_TIMEOUT)
+            writable_connections = select_for_write(readable_connections, POLL_TIMEOUT)
             for client_socket in writable_connections:
                 log_connection(client_socket, f"Sending back {SERVER_REPLY}")
                 try:
                     client_socket.sendall(SERVER_REPLY.encode())
                 except OSError as err:
                     log_connection(client_socket, f"An error has occurred: {err}")
+                    close_connection(clients, client_socket)
 
 
 if __name__ == '__main__':
